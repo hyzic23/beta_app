b6cd1eb7908fc8a436355cd284668ef63c2c05df
hyzic <isaac.oghenevwapo@gmail.com>
2024-10-28 12:18:46 -0400

    First Commit with Login Page

---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text('Login & Signup'),
+            bottom: const TabBar(
+              tabs: [Tab(text: 'Login'), Tab(text: 'Signup')],
+            ),
+          ),
+          body: TabBarView(
+            children: [
+              LoginCard(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+//Login
+class LoginCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Card(
+        margin: EdgeInsets.all(20.0),
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              TextField(
+                decoration: InputDecoration(labelText: 'Email'),
+              ),
+              SizedBox(height: 10),
+              TextField(
+                decoration: InputDecoration(labelText: 'Password'),
+                obscureText: true,
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text('Login'),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
